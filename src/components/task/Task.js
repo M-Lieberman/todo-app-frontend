ba0f922833810b69af9aa57d6022209d7b67b03f
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-10-03 18:19:07 +0100

    add Task component and refactor into task folder

--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
@@ -8,26 +7,25 @@ function Task({ tasks }) {
 
     return (
 
-        <Container>
+        tasks.map(task =>
             <Row>
                 <Col>
-                    Description: {tasks.taskDescription}
-                    {/* {!task.completed &&
+                    Description: {task.taskDescription}
+                </Col>
+                <Col>
+                    {!task.completed &&
                         <Button type="button" className="primary" >
                             DONE
                     </Button>
-                    } */}
-                    column 1
-                </Col>
-                <Col>
-
-                    {/* <Button type="button" className="danger" >
+                    }
+                    <Button type="button" className="danger" >
                         DELETE
-                </Button> */}
-                column 2
-            </Col>
+                </Button>
+                </Col>
             </Row>
-        </Container>
+
+        )
+
 
     );
 }
