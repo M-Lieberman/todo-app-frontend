8a0b44d642280520de5767e7e205f57530fba7f0
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-10-04 11:36:52 +0100

    add Task unit test. Skip default App test

--- src/components/task/task.test.js
+++ src/components/task/task.test.js
@@ -3,21 +3,38 @@ import { render } from '@testing-library/react';
 import Task from './Task';
 
 describe('Task component', () => {
+
     test(`Given the required props,
     When the component is rendered,
     Then it should render the Task text`, () => {
             const exampleText = "This is a test task";
 
             const requiredProps = {
-                tasks: [{
+                tasks: {
                     taskDescription: exampleText,
                     completed: false
-                },]
+                }
             }
 
-            const { getByText } = render(<Task {...requiredProps} />)
-            expect(getByText(exampleText)).ToBeTruthy();
+            const { getByText } = render(<Task {...requiredProps} />);
+            expect(getByText(exampleText)).toBeTruthy();
         })
+
+    test(`Given the required props,
+    When the component is rendered,
+    Then it should not render a delete button`, () => {
+        const requiredProps = {
+            tasks: {
+                taskDescription: "This is a test task",
+                completed: true
+            }
+        }
+        const { getAllByRole }  = render(<Task {...requiredProps} />);
+    expect(getAllByRole('button').find(button => button.textContent === 'DELETE')).toBeTruthy();
+    })
+
+
+
 
     })
 
