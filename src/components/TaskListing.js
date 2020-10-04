1f85691b6535322eabe2e8e1311f88c3b14da5c6
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-10-04 11:21:50 +0100

    refactor tasks to map in TaskListing

--- src/components/TaskListing.js
+++ src/components/TaskListing.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+    
 import Task from './task/Task';
 
 function TaskListing({ taskData }) {
@@ -11,40 +14,21 @@ function TaskListing({ taskData }) {
         <Container>
             <Row>
                 <Col >
-                <h5>Incomplete Tasks</h5>
-                    <Task tasks={incompleteTasks} />
+                    <h5>Incomplete Tasks</h5>
+                    {incompleteTasks.map((task) =>
+                        <Task tasks={task} />
+                    )}
                 </Col>
                 <Col>
-                <h5>Completed Tasks</h5>
-                    <Task tasks={completedTasks} />
+                    <h5>Completed Tasks</h5>
+                    {completedTasks.map((task) =>
+                        <Task tasks={task} />
+                    )}
+
                 </Col>
             </Row>
         </Container>
     );
 }
-
-
-// function TaskListing({taskData}) {
-
-//     const incompleteTasks = taskData.filter((task) => task.completed === false);
-//     const completedTasks = taskData.filter((task) => task.completed === true);
-
-//     return (
-//         <Grid container spacing={3}>
-//             <Grid item xs={12} md={6}>
-//                 <Typography variant="h6">Tasks to do:</Typography>
-//                 {incompleteTasks.map((task) =>
-//                     <Task task={task} />
-//                 )}
-//             </Grid>
-//             <Grid item xs={12} md={6}>
-//                 <Typography variant="h6">Complete tasks:</Typography>
-//                 {completedTasks.map((task) => 
-//                         <Task task={task} />
-//                 )}
-//             </Grid>
-//         </Grid>
-//     );
-// }
 
 export default TaskListing;
--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -1,31 +1,52 @@
 import React from 'react';
-import { Button, Row, Col } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Task({ tasks }) {
 
     return (
-
-        tasks.map(task =>
-
-            <Row >
-                <Col md={6}>
-                    {task.taskDescription}
-                </Col>
-                <Col md={3}>
-                    {!task.completed &&
-                        <Button type="button" variant="primary" >
-                            DONE
+        <Row >
+            <Col md={6}>
+                {tasks.taskDescription}
+            </Col>
+            <Col md={3}>
+                {!tasks.completed &&
+                    <Button type="button" variant="primary" >
+                        DONE
                     </Button>
-                    }
-                </Col>
-                <Col md={3}>
-                    <Button type="button" variant="danger" >
-                        DELETE
+                }
+            </Col>
+            <Col md={3}>
+                <Button type="button" variant="danger" >
+                    DELETE
                 </Button>
-                </Col>
-            </Row>
-        )
+            </Col>
+        </Row>
     );
+
+    // return (
+    //     tasks.map(task =>
+
+    //         <Row >
+    //             <Col md={6}>
+    //                 {task.taskDescription}
+    //             </Col>
+    //             <Col md={3}>
+    //                 {!task.completed &&
+    //                     <Button type="button" variant="primary" >
+    //                         DONE
+    //                 </Button>
+    //                 }
+    //             </Col>
+    //             <Col md={3}>
+    //                 <Button type="button" variant="danger" >
+    //                     DELETE
+    //             </Button>
+    //             </Col>
+    //         </Row>
+    //     )
+    // );    
 }
 
 export default Task;
