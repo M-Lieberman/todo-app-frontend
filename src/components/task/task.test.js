068e6e185e051beb1939584606a1f25504b2a1f8
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-10-04 11:42:53 +0100

    more unit test for Task

--- src/components/task/task.test.js
+++ src/components/task/task.test.js
@@ -33,8 +33,32 @@ describe('Task component', () => {
     expect(getAllByRole('button').find(button => button.textContent === 'DELETE')).toBeTruthy();
     })
 
+    test(`Given the required props,
+    When the component is rendered,
+    Then it should render a Done button`, () => {
+        const requiredProps = {
+            tasks: {
+                taskDescription: "This is a test task",
+                completed: false
+            }
+        }
+        const { getAllByRole }  = render(<Task {...requiredProps} />);
+    expect(getAllByRole('button').find(button => button.textContent === 'DONE')).toBeTruthy();
+    })
 
+    test(`Given the required props,
+    When the component is rendered,
+    Then it should not render a Done button`, () => {
+        const requiredProps = {
+            tasksq: {
+                taskDescription: "This is a test task",
+                completed: true
+            }
+        }
+        const {getAllByRole} = render(<Task {...requiredProps} />);
+        expect(getAllByRole('button').find(button => button.textContent === 'DONE')).toBeFalsy();
 
+    })
 
     })
 
